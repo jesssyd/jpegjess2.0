cbb12f27bf60e87fe6b17020792bb36e0060a5d1
jesssyd <tidd.jessica.s@gmail.com>
2025-05-18 20:34:50 -0400

    Ughhh

--- src/app/galleryPage/components/Header.tsx
+++ src/app/galleryPage/components/Header.tsx
@@ -2,20 +2,11 @@ export default function Header() {
   return (
     <section>
       <div className="section page-container columns lg:mb-16 md:mb-12 mb-8">
-        <div className="lg:col-span-6 col-span-2">
-          <h1>
+        <div className="col-span-full">
+          <h1 >
             jpegjess<span>_</span>
           </h1>
         </div>
-        {/* <div className="col-start-4 col-span-3 lg:col-start-7 lg:col-span-auto">
-          <p className="caption mb-2">
-            i bought my first point and shoot in 2022, which got stolen 3 months
-            later
-          </p>
-          <p className="caption mb-2">
-            currently shooting on a konica zoom fr735r
-          </p>
-        </div> */}
       </div>
     </section>
   );
